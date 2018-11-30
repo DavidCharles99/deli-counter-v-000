31dd555b347f5a317d990be4657ff091eb897892
David Charles <ratlover03@gmail.com>
2018-11-30 18:12:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+katz_deli = []
+
+def line(name, counter)
+  if line.empty? = true
+    puts "There is nobody waiting to be served!"
+  else
+    current_line = "there is currenty:"
